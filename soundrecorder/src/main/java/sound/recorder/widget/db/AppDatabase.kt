0f53f67e3c6bbfa70d04df80ee3b705a7e63195e
asywalulfikri <asywalul.fikri@ottodigital.id>
2023-05-18 23:37:47 +0700

    update AppDatabase.kt

--- soundrecorder/src/main/java/sound/recorder/widget/db/AppDatabase.kt
+++ soundrecorder/src/main/java/sound/recorder/widget/db/AppDatabase.kt
@@ -3,7 +3,7 @@ package sound.recorder.widget.db
 import androidx.room.Database
 import androidx.room.RoomDatabase
 
-@Database(entities = [AudioRecord::class], version = 3)
+@Database(entities = [AudioRecord::class], version = 1)
 internal abstract class AppDatabase : RoomDatabase(){
     abstract fun audioRecordDAO(): AudioRecordDAO
 }
